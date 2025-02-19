2727e1890c03e1a8f64f321f968e0a81834ec428
Nsengimana François <francknsengimana@gmail.com>
2025-02-19 20:02:50 +0100

    mise en place de l'environnement

---
+++ config/global.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_NAME', 'database');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'db');
+?>
---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<script type="application/javascript" scr="/assets/js/jquery-3.2.1.slim.min.js"></script>
+<script type="application/javascript" scr="/assets/js/docs.min.js"></script>
+</body>
+</html>
